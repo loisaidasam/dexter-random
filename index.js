b9e50e162a7c83ac602ef1733b3dc4dd477303f0
Sam Sandberg <sam.sandberg@gmail.com>
2015-09-19 15:17:47 -0400

    Initial working version (maybe?)

--- index.js
+++ index.js
@@ -6,8 +6,13 @@ module.exports = {
      * @param {AppData} dexter Container for all data used in this workflow.
      */
     run: function(step, dexter) {
-        var results = { foo: 'bar' };
+        function randomIntInc (low, high) {
+            return Math.floor(Math.random() * (high - low + 1) + low);
+        }
+        var low = step.input('low');
+        var high = step.input('high');
+        var result = { result: randomIntInc(low, high) };
         //Call this.complete with the module's output.  If there's an error, call this.fail(message) instead.
-        this.complete(results);
+        this.complete(result);
     }
 };
